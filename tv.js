18eee43ad83a1368a80b6bf72a73b7beadf0edb8
joeshu <350027830@qq.com>
2026-01-28 22:24:44 +0800

    Update tv.js

--- tv.js
+++ tv.js
@@ -16,9 +16,9 @@
 ^https?:\/\/yzy0916\.q8nsderug\.com\/v2\/api\/adInfo\/getTextAd url reject-200
 ^https?:\/\/yzy0916\.q8nsderug\.com\/v2\/api\/vodInfo\/getVodBodyAd url reject-200
 
-^https?:\/\/yz*\.*\.com\/v2\/api\/adInfo url reject-200
+^https?:\/\/yz*\.*\.com\/v2\/api\/adInfo\/* url reject-200
 ^https?:\/\/yz*\.*\.com\/v2\/api\/vodInfo\/getVodBodyAd url reject-200
-
+^https?:\/\/yz*\.*\.com\/v2\/api\/home\/notice url reject-dict
 [mitm]
 hostname = yzy*.*.com,yz1018.*.com,yz250907.*.com,
 *************************************/
