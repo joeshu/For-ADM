8478ecfb31d93de58af328b6d1e7d661c4985956
joeshu <350027830@qq.com>
2026-01-28 23:28:19 +0800

    Update tv2.js

--- tv2.js
+++ tv2.js
@@ -2,7 +2,8 @@
 /****************************************
 [rewrite_local]
 ^https?:\/\/yzy0916\.q8nsderug\.com\/v2\/api\/home\/firstScreen url script-response-body https://raw.githubusercontent.com/joeshu/For-ADM/refs/heads/master/tv2.js
-
+^https?:\/\/yz250907\.*\.com\/v2\/api\/home\/firstScreen url script-response-body https://raw.githubusercontent.com/joeshu/For-ADM/refs/heads/master/tv2.js
+^https?:\/\/yz1018\.*\.com\/v2\/api\/home\/firstScreen url script-response-body https://raw.githubusercontent.com/joeshu/For-ADM/refs/heads/master/tv2.js
 [mitm]
 hostname = yzy*.*.com,yz1018.*.com,yz250907.*.com,
 *************************************/
