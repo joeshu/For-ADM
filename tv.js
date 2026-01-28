7282a4db36c5783b82f8d4959ab34d7b2ad36a2f
joeshu <350027830@qq.com>
2026-01-28 20:25:36 +0800

    Update tv.js

--- tv.js
+++ tv.js
@@ -1,11 +1,13 @@
 
 /****************************************
 [rewrite_local]
-^https:\/\/yzy*\.*\.com\/v2\/api\/basic\/init url script-response-body https://raw.githubusercontent.com/joeshu/For-ADM/refs/heads/master/tv.js
+^https?:\/\/yzy0916\.q8nsderug\.com\/v2\/api\/basic\/init url script-response-body https://raw.githubusercontent.com/joeshu/For-ADM/refs/heads/master/tv.js
+^https?:\/\/yzy0916\.q8nsderug\.com\/v2\/api\/basic\/init url response-body "startAdShowTime":5," response-body "startAdShowTime":0,"
 [mitm]
 hostname = yzy*.*.com
 *************************************/
-/* ^https:\/\/yzy*\.*\.com\/v2\/api\/basic\/init  url response-body "startAdShowTime":?", response-body "startAdShowTime":0" */
+
+
 var body = $response.body;
 var obj = JSON.parse(body);
 
