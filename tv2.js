b5bfc2d05debf54c7049cb39c976e7339c2c1091
joeshu <350027830@qq.com>
2026-01-28 23:00:38 +0800

    Create tv2.js

---
+++ tv2.js
@@ -0,0 +1,20 @@
+
+/****************************************
+[rewrite_local]
+^https?:\/\/yzy0916\.q8nsderug\.com\/v2\/api\/home\/firstScreen url script-response-body https://raw.githubusercontent.com/joeshu/For-ADM/refs/heads/master/tv2.js
+
+[mitm]
+hostname = yzy*.*.com,yz1018.*.com,yz250907.*.com,
+*************************************/
+
+var body = $response.body;
+var obj = JSON.parse(body);
+
+
+obj.data["focusAdList"] = null;
+
+body = JSON.stringify(obj);
+
+console.log(body);
+
+$done(body);
