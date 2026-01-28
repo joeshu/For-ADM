3eda29aa8cc662a8f01ac908572c807b097d659f
joeshu <350027830@qq.com>
2026-01-28 22:04:40 +0800

    Update tv.js

--- tv.js
+++ tv.js
@@ -12,6 +12,13 @@
 ^https?:\/\/yz1018\.tgs2hl4ut\.com\/v2\/api\/basic\/init url script-response-body https://raw.githubusercontent.com/joeshu/For-ADM/refs/heads/master/tv.js
 ^https?:\/\/yz1018\.goi6lhmry\.com\/v2\/api\/basic\/init url script-response-body https://raw.githubusercontent.com/joeshu/For-ADM/refs/heads/master/tv.js
 ^https?:\/\/yz1018\.6vh3qyu9x\.com\/v2\/api\/basic\/init url script-response-body https://raw.githubusercontent.com/joeshu/For-ADM/refs/heads/master/tv.js
+
+^https?:\/\/yzy0916\.q8nsderug\.com\/v2\/api\/adInfo\/getTextAd url reject-200
+^https?:\/\/yzy0916\.q8nsderug\.com\/v2\/api\/vodInfo\/getVodBodyAd url reject-200
+
+^https?:\/\/yz*\.*\.com\/v2\/api\/adInfo\/getTextAd url reject-200
+^https?:\/\/yz*\.*\.com\/v2\/api\/vodInfo\/getVodBodyAd url reject-200
+
 [mitm]
 hostname = yzy*.*.com,yz1018.*.com,yz250907.*.com,
 *************************************/
