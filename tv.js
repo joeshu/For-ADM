e67f8e9a0c8fe425e118f45fdbceae70e266d20b
joeshu <350027830@qq.com>
2026-01-28 21:20:14 +0800

    Update tv.js

--- tv.js
+++ tv.js
@@ -1,8 +1,8 @@
 
 /****************************************
 [rewrite_local]
-^https?:\/\/yzy0916\.q8nsderug\.com\/v2\/api\/basic\/init url script-response-body https://raw.githubusercontent.com/joeshu/For-ADM/refs/heads/master/tv.js
-^https?:\/\/yzy0916\.q8nsderug\.com\/v2\/api\/basic\/init url response-body data."startAdShowTime" : 5, response-body data."startAdShowTime" : 0,
+^https?:\/\/yzy*\.*\.com\/v2\/api\/basic\/init url script-response-body https://raw.githubusercontent.com/joeshu/For-ADM/refs/heads/master/tv.js
+
 [mitm]
 hostname = yzy*.*.com
 *************************************/
