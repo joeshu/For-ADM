d7e1328b57b09bc925b1c9dcb74ad5705e2d28a4
joeshu <350027830@qq.com>
2026-01-29 17:19:40 +0800

    Update tv2.js

--- tv2.js
+++ tv2.js
@@ -1,26 +1,8 @@
 
 /****************************************
 [rewrite_local]
-^https?:\/\/(yzy0916|yz1018|yz250907)\.(q8nsderug|6vh3qyu9x|goi6lhmry|tgs2hl4ut|o5r52at9v|hzkeka49|jba2yo1c|3nyk7h9o|7k5jb8t9|zazy3mc5)\.com\/v2\/api\/home\/firstScreen url script-response-body https://raw.githubusercontent.com/joeshu/For-ADM/refs/heads/master/tv2.js
-^https?:\/\/(yzy0916|yz1018|yz250907)\.(q8nsderug|6vh3qyu9x|goi6lhmry|tgs2hl4ut|o5r52at9v|hzkeka49|jba2yo1c|3nyk7h9o|7k5jb8t9|zazy3mc5)\.com\/v1\/api\/adInfo\/getPageAd url script-response-body https://raw.githubusercontent.com/joeshu/For-ADM/refs/heads/master/tv2.js
-
+^https?:\/\/nkas\.lnaahhj\.com\/icon\/betatest\.png* url reject-200
+^https?:\/\/ansj\.ejjjaakq\.com\/api\/(init|index\/ad) url request-body "ad_type":"2", request-body "ad_type":"0",
 [mitm]
-hostname = yzy*.*.com,yz1018.*.com,yz250907.*.com,
+hostname =ansj.ejjjaakq.com
 *************************************/
-
-var body = $response.body;
-var obj = JSON.parse(body);
-
-/*
-obj.data["focusAdList"] = null;
-obj.data["floatAd"] = null;
-*/
-
-delete obj.data.focusAdList;
-delete obj.data.floatAd;
-
-body = JSON.stringify(obj);
-
-console.log(body);
-
-$done(body);
