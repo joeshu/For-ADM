93d1f4e3ec80656cf096e6b45a3d431d721bd362
joeshu <350027830@qq.com>
2026-01-29 23:22:35 +0800

    Update tv.js

--- tv.js
+++ tv.js
@@ -25,13 +25,77 @@ obj.data["startAdList"] = null;
 
 /*************************************/
 if ($request.url.indexOf(path2) != -1){
-delete obj.data.focusAdList;
+delete obj.data.focusAdList;   
+obj.data.hotMudleList = [
+      {
+        "imgUrl" : "https://ff1.1t4b19ec.com/upload/images/202405/799e33c8-107e-4adc-af5f-af6edf3fe9e4.png",
+        "adImgType" : 0,
+        "adAction" : 9,
+        "id" : 59,
+        "actionContent" : "",
+        "title" : "上映表单",
+        "adInsertRule" : null,
+        "subTitle" : "",
+        "fileType" : 1
+      },
+      {
+        "imgUrl" : "https://ff1.1t4b19ec.com/upload/images/202405/d294ae3a-4e06-464d-a1c5-0738e12a2e4b.png",
+        "adImgType" : 0,
+        "adAction" : 3,
+        "id" : 6,
+        "actionContent" : "2",
+        "title" : "爱优腾芒",
+        "adInsertRule" : null,
+        "subTitle" : "",
+        "fileType" : 1
+      },
+      {
+        "imgUrl" : "https://ff1.1t4b19ec.com/upload/images/202405/f7c78ea1-e9af-4bf8-835d-07b0509b7120.png",
+        "adImgType" : 0,
+        "adAction" : 3,
+        "id" : 7,
+        "actionContent" : "3",
+        "title" : "院线上新",
+        "adInsertRule" : null,
+        "subTitle" : "",
+        "fileType" : 1
+      },
+      {
+        "imgUrl" : "https://ff1.1t4b19ec.com/upload/images/202405/291a8fc5-96f1-4f68-a09d-5d47e10bfb8a.png",
+        "adImgType" : 0,
+        "adAction" : 3,
+        "id" : 8,
+        "actionContent" : "4",
+        "title" : "美韩日剧",
+        "adInsertRule" : null,
+        "subTitle" : "",
+        "fileType" : 1
+      },
+      {
+        "imgUrl" : "https://ff1.1t4b19ec.com/upload/images/202405/2e3cbeff-b707-4d39-a1df-2d93efe336ae.png",
+        "adImgType" : 0,
+        "adAction" : 7,
+        "id" : 9,
+        "actionContent" : "",
+        "title" : "热播排行",
+        "adInsertRule" : null,
+        "subTitle" : "",
+        "fileType" : 1
+      }]
 }
 
 /*************************************/
 if ($request.url.indexOf(path3) != -1){
 delete obj.data.floatAd;
 }
+
+
+
+
+
+
+
+
 
 body = JSON.stringify(obj);
 console.log(body);
