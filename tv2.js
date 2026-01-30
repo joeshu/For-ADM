6307e3597e70567e7961f1ec7df3269afa05d998
joeshu <350027830@qq.com>
2026-01-30 15:55:15 +0800

    Update tv2.js

--- tv2.js
+++ tv2.js
@@ -1,8 +1,13 @@
 
 /****************************************
 [rewrite_local]
+##yqs1.app
 ^https?:\/\/nkas\.lnaahhj\.com\/icon\/betatest\.png* url reject-200
-^https?:\/\/ansj\.ejjjaakq\.com\/api\/(init|index\/ad) url request-body "ad_type":"2", request-body "ad_type":"0",
+^https?:\/\/ansj\.ejjjaakq\.com\/api\/(init|index\/ad) url request-body "ad_type":"2", request-body "ad_type":"0"
+##瓜子影视
+^https?:\/\/(api|gzapi)\..+\.com\/App\/[Aa]d\/(barsIndexAdInfo|splashInfo|vajraInfo|config|activityInfo|skitAdInfo) url reject-200
+^https?:\/\/api\..+.com\/App\/(IndexList|IndexPlay|Ad)\/(homeFloatAd|barsPlayAdInfo|vodAdvertisement) url reject-200
+
 [mitm]
 hostname =ansj.ejjjaakq.com
 *************************************/
