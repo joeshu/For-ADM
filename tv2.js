6de1667ad204a830b93e4ba5e3f32bfb677d47ad
joeshu <350027830@qq.com>
2026-01-30 16:58:34 +0800

    Update tv2.js

--- tv2.js
+++ tv2.js
@@ -4,7 +4,7 @@
 ##yqs1.app 一起刷影视
 ^https?:\/\/nkas\.lnaahhj\.com\/icon\/betatest\.png* url reject-200
 ^https?:\/\/ansj\.ejjjaakq\.com\/api\/(init|index\/ad) url request-body "ad_type":"2", request-body "ad_type":"0"
-##https://js.anksna.com:8888/filejs/assets/LazyImg.vue_vue_type_script_setup_true_lang-JYdz6qQc.js
+^https://js.anksna.com:8888/filejs/assets/LazyImg.vue_vue_type_script_setup_true_lang-.+.js url 302 
 
 ^https?:\/\/yqs1\.app url response-body var product_type = '\d' response-body var product_type = '0'
 ##瓜子影视
