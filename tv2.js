32b29372481ec5be877e4cb0265e959092ac954d
joeshu <350027830@qq.com>
2026-01-30 15:56:34 +0800

    Update tv2.js

--- tv2.js
+++ tv2.js
@@ -1,7 +1,7 @@
 
 /****************************************
 [rewrite_local]
-##yqs1.app
+##yqs1.app 一起刷影视
 ^https?:\/\/nkas\.lnaahhj\.com\/icon\/betatest\.png* url reject-200
 ^https?:\/\/ansj\.ejjjaakq\.com\/api\/(init|index\/ad) url request-body "ad_type":"2", request-body "ad_type":"0"
 ##瓜子影视
