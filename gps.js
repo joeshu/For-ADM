ae03b0549aa029bba01932c0340ac1bbbaec8981
joeshu <350027830@qq.com>
2026-01-31 10:10:59 +0800

    Create gps.js

---
+++ gps.js
@@ -0,0 +1,28 @@
+/* * *
+脚本功能：gps工具箱
+软件版本：2.7.38
+*******************************
+[rewrite_local]
+# > gps工具箱
+^^https:\/\/service\.gpstool\.com\/app\/index\/getUserInfo url script-response-body https://raw.githubusercontent.com/WeiGiegie/666/main/gps.js
+
+[mitm]
+hostname = service.gpstool.com
+***/
+varobj = JSON.prase($response.body);
+if(obj&&obj.data){
+
+
+  
+}
+var obj = JSON.parse($response.body);
+if (obj && obj.data) {
+    obj.data.is_vip = 1;
+    obj.data.vip_name = "VIP会员";
+    obj.data.vip_expire_date = 2099999;
+    obj.data.is_super_vip = 1;
+    obj.data.is_power_vip = 1;
+    obj.data.group_vip = 1;
+    obj.data.group_vip_expire_date = 2099999;
+}
+$done({body: JSON.stringify(obj)});
