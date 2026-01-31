5d5592e3b74a058f2ca211685950c5d8ae96e5c2
joeshu <350027830@qq.com>
2026-01-31 13:25:52 +0800

    Update qiujingapp.js

--- qiujingapp.js
+++ qiujingapp.js
@@ -1,9 +1,9 @@
 /****************************************
 [rewrite_local]
 # 移除球竞弹窗推广
-^https?:\/\/gateway-api\.yizhilive\.com\/api\/v2\/index\/carouses\/(8|11|6|3)(\?.*)?$ response-body-json-jq 'if (.data|type)=="array" then .data=[] else . end'
+^https?:\/\/gateway-api\.yizhilive\.com\/api\/v2\/index\/carouses\/(8|11|6|3)(\?.*)?$ url script-response-body https://raw.githubusercontent.com/joeshu/For-ADM/refs/heads/master/gps.js
 # 移除球竞轮播广告
-^https?:\/\/gateway-api\.yizhilive\.com\/api\/v3\/index\/all\?.*position=2.*$ response-body-json-jq 'if (.data|type)=="object" and (.data.banners|type)=="array" then .data.banners=[] else . end'
+^https?:\/\/gateway-api\.yizhilive\.com\/api\/v3\/index\/all\?.*position=2.*$ url script-response-body https://raw.githubusercontent.com/joeshu/For-ADM/refs/heads/master/gps.js
 
 
 [mitm]
