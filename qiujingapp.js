4c532ef04507fcbff630e82631028a58e7e48706
joeshu <350027830@qq.com>
2026-01-31 13:45:59 +0800

    Update qiujingapp.js

--- qiujingapp.js
+++ qiujingapp.js
@@ -4,11 +4,9 @@
 ^https?:\/\/gateway-api\.yizhilive\.com\/api\/v2\/index\/carouses\/(8|11|6|3)(\?.*)?$ url script-response-body https://raw.githubusercontent.com/joeshu/For-ADM/refs/heads/master/qiujingapp.js
 # 移除球竞轮播广告
 ^https?:\/\/gateway-api\.yizhilive\.com\/api\/v3\/index\/all\?.*position=2.*$ url script-response-body https://raw.githubusercontent.com/joeshu/For-ADM/refs/heads/master/qiujingapp.js
-
-
 [mitm]
 hostname= gateway-api.yizhilive.com
-***************************/
+****************************************/
 const url =  $request.url;
 const path2 = "/v3/index/all?page=1&pageSize=20&position=2";
 var body = $response.body;
