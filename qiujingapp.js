aa64070c23b8c7d6dea23bb8a1337d7386dff722
joeshu <350027830@qq.com>
2026-01-31 16:48:31 +0800

    Update qiujingapp.js

--- qiujingapp.js
+++ qiujingapp.js
@@ -8,58 +8,41 @@
 hostname= gateway-api.yizhilive.com
 ****************************************/
 const url =  $request.url;
-const path3 = "/api/v2/index/carouses/8";
-const path4 = "/api/v2/index/carouses/11";
-const path5 = "/api/v2/index/carouses/6";
-const path6 = "/api/v2/index/carouses/3";
+//const path6 = "/api/v2/index/carouses/3";
 const path2 = "/v3/index/all?page=1&pageSize=20&position=2";
 var body = $response.body;
 var obj = JSON.parse(body);
 
 /********************************************/
 if (url.match(/\/api\/v2\/index\/carouses\/(11|8|6|3)\b/)) {
-
-   console.log(`匹配到URL: ${url}`);
+//console.log(`匹配到URL: ${url}`);
  if (Array.isArray(obj.data)) {
     obj.data = [];
   }
 }
+
 /*
-if ($request.url.indexOf(path3) != -1){
-  if (Array.isArray(obj.data)) {
-    obj.data = [];
- }
-}
-if ($request.url.indexOf(path4) != -1){
-  if (Array.isArray(obj.data)) {
-    obj.data = [];
- }
-}
-if ($request.url.indexOf(path5) != -1){
-  if (Array.isArray(obj.data)) {
-    obj.data = [];
- }
-}
 if ($request.url.indexOf(path6) != -1){
   if (Array.isArray(obj.data)) {
     obj.data = [];
  }
 }
-
-/************************************************
-if (url.match(/\/api\/v3\/index\/all\?.*position=2/gi))) {
-  if(obj.data&&obj.data.banners){
+*/
+/************************************************/
+if (url.match(/\/api\/v3\/index\/all\?.*position=2.*$/)) {
+  console.log(`匹配到URL: ${url}`);
+   if(obj.data&&obj.data.banners){
   obj.data.banners = [];
 }
 }
-*/
 
+/*
 if ($request.url.indexOf(path2) != -1){
   if(obj.data&&obj.data.banners){
   obj.data.banners = [];
  }
 }
-
+*/
 
 body = JSON.stringify(obj);
 console.log(body);
