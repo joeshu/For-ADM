ae75bf62e1427881339e9381584bca0a99b10f86
joeshu <350027830@qq.com>
2026-01-31 13:42:44 +0800

    Update qiujingapp.js

--- qiujingapp.js
+++ qiujingapp.js
@@ -9,24 +9,32 @@
 [mitm]
 hostname= gateway-api.yizhilive.com
 ***************************/
-
-const path1 = "/v2/index/carouses/(3|6|8|11)";
-const path2 = "/v3/index/all?page=1&pageSize=20&position=2";
+const url =  $request.url;
+//const path2 = "/v3/index/all?page=1&pageSize=20&position=2";
 var body = $response.body;
 var obj = JSON.parse(body);
 
 /********************************************/
-if ($request.url.indexOf(path1) != -1){
-  if(obj.data){
-  obj.data = [];
+
+ if (url.match(api\/v2\/index\/carouses\/(8|11|6|3)(\?|$)/)) {
+  if (Array.isArray(obj.data)) {
+    obj.data = [];
   }
 }
+
 /************************************************/
-if ($request.url.indexOf(path2) != -1){
+ if (url.match(api\/v3\/index\/all\?.*position=2.*$)) {
   if(obj.data&&obj.data.banners){
   obj.data.banners = [];
   }
 }
+
+
+//if ($request.url.indexOf(path2) != -1){
+//  if(obj.data&&obj.data.banners){
+//  obj.data.banners = [];
+//  }
+//}
 
 body = JSON.stringify(obj);
 console.log(body);
