14034e3029f93e46c9da06d891e9f9b8a182767c
joeshu <350027830@qq.com>
2026-01-31 15:04:05 +0000

    Update TV3.js

--- TV3.js
+++ TV3.js
@@ -9,7 +9,7 @@ var obj = JSON.parse(body);
 
 
 if (obj.data) {
-    obj.data.vipLevel = 1;
+    obj.data.vipLevel = 2;
     obj.data.check_in_days_cycle = 1000;
     obj.data.check_in_days_continue = 100;
     obj.data.reset_card = 101;
@@ -21,6 +21,10 @@ if (obj.data) {
     obj.data.check_in_days_all = 200;
     obj.data.partner = 2000;
     obj.data.score = 20000;
+    obj.data.buyProducts = ["product_id":3,"product_name":"年卡会员","product_type":1,"product_type_sub":3];
+
+
+    
 }
 
 body = JSON.stringify(obj);
