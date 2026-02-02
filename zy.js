4aa261301d50a01d5796a20bf23c26955fb0cb96
joeshu <350027830@qq.com>
2026-02-02 10:23:15 +0800

    Create zy.js

---
+++ zy.js
@@ -0,0 +1,6 @@
+# > 微信公众号
+# hostname = mp.weixin.qq.com
+# 公众号文章下方广告
+^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad url response-body advertisement response-body fmz200
+# 微信公众号去除商品推广
+^https?:\/\/mp\.weixin\.qq\.com\/mp\/cps_product_info\?action url reject-dict
