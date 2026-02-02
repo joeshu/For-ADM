f9df69c03d4bd575e9892317d6c2c6cd58573a2f
joeshu <350027830@qq.com>
2026-02-02 10:27:46 +0800

    Update zy.js

--- zy.js
+++ zy.js
@@ -1,6 +1,8 @@
 # > 微信公众号
-# hostname = mp.weixin.qq.com
+[rewrite_local]
 # 公众号文章下方广告
 ^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad url response-body advertisement response-body fmz200
 # 微信公众号去除商品推广
 ^https?:\/\/mp\.weixin\.qq\.com\/mp\/cps_product_info\?action url reject-dict
+[mitm]
+hostname = mp.weixin.qq.com
