59658d34106efdc0a03d9ceaf9819d9f8f88df03
joeshu <350027830@qq.com>
2026-02-09 16:56:33 +0800

    Update TV3.js

--- TV3.js
+++ TV3.js
@@ -39,6 +39,39 @@ if ($request.url.indexOf(path4) != -1){
 obj.data.adList.shift();
 }
 
+/*************************
+const url = $request.url;
+if (!$response.body) $done({});
+let obj = JSON.parse($response.body);
+
+if (
+    url.includes("/api/news/index") ||
+    url.includes("/api/topmenu/getfeeds")
+) {
+  if (obj?.data?.list?.length > 0) {
+    let list = obj.data.list;
+    const newList = [];
+    for (let item of list) {
+      if (item?.feedContent?.smallTags?.some((i) =>
+          i?.text?.includes("广告"))
+      ) {
+        continue;
+      }
+      if ([10002, 10003].includes(item?.feedType)) {
+        continue;
+      }
+      newList.push(item);
+    }
+    obj.data.list = newList;
+  }
+}
+
+$done({body: JSON.stringify(obj)})
+*****///
+
+
+
+
 body = JSON.stringify(obj);
 console.log(body);
 $done(body);
