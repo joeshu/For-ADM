6b5ea6e96de0c46cf3bcb19dbecc15463d89e245
joeshu <350027830@qq.com>
2026-02-09 17:19:05 +0800

    Update tv.js

--- tv.js
+++ tv.js
@@ -1,7 +1,7 @@
 
 /****************************************
 [rewrite_local]
-##^https?:\/\/(yzy0916|yz1018|yz250907|yz0320|cfvip)\..+\.com\/(v2|v1)\/api\/(basic\/init|home\/firstScreen|adInfo\/getPageAd|home\/body) url script-response-body https://raw.githubusercontent.com/joeshu/For-ADM/refs/heads/master/tv.js
+^https?:\/\/(yzy0916|yz1018|yz250907|yz0320|cfvip)\..+\.com\/(v2|v1)\/api\/(basic\/init|home\/firstScreen|adInfo\/getPageAd|home\/body) url script-response-body https://raw.githubusercontent.com/joeshu/For-ADM/refs/heads/master/tv.js
 ^https?:\/\/(yz\w{4,6}|cfvip)\..+\.com\/(v2|v1)\/api\/(adInfo|vodInfo)\/(getPageAd|getTextAd|getVodBodyAd|getVodPauseAd|getUserCenterAd) url reject-200
 ^https?:\/\/(yz\w{4,6}|cfvip)\..+\.com\/(v2|v1)\/api\/(home\/notice|gameCenter\/gameDetailList) url reject-200
 
@@ -15,8 +15,6 @@ const path4 = "/home/body";
 
 var body = $response.body;
 var obj = JSON.parse(body);
-//^https?:\/\/(yz\w{4,6}|cfvip)\..+\.com\/(v2|v1)\/api\/(basic\/init|home\/firstScreen|adInfo\/getPageAd) url script-response-body https://raw.githubusercontent.com/joeshu/For-ADM/refs/heads/master/tv.js
-
 
 /*************************************/
 if ($request.url.indexOf(path1) != -1){
@@ -30,7 +28,20 @@ if ($request.url.indexOf(path2) != -1){
 delete obj.data.focusAdList; 
 //obj.data.hotMudleList.pop();
 //obj.data.hotMudleList = obj.data.hotMudleList.slice(0, -5);
-obj.data.hotMudleList = obj.data.hotMudleList.slice(0, 5);
+//obj.data.hotMudleList = obj.data.hotMudleList.slice(0, 5);
+if (obj?.data?.hotMudleList?.length > 0) {
+    let list = obj.data.hotMudleList;
+    const newList = [];
+    for (let item of list) {
+      if (item?.title?.some((i) =>
+          i?.text?.includes("广告招商"))
+      ) {
+        continue;
+      }
+      newList.push(item);
+    }
+    obj.data.hotMudleList = newList;
+  }
 }
 
 /*************************************/
@@ -40,8 +51,52 @@ delete obj.data.popupAd;
 }
 
 if ($request.url.indexOf(path4) != -1){
-obj.data.adList.shift();
+//obj.data.adList.shift();
+if (obj?.data?.adList?.length > 0) {
+    let list = obj.data.adList;
+    const newList = [];
+    for (let item of list) {
+      if (item?.title?.some((i) =>
+          i?.text?.includes("招商"))
+      ) {
+        continue;
+      }
+      newList.push(item);
+    }
+    obj.data.adList = newList;
+  }
 }
+
+/*************************
+const url = $request.url;
+if (!$response.body) $done({});
+let obj = JSON.parse($response.body);
+
+if (
+    url.includes("/api/news/index") ||
+    url.includes("/api/topmenu/getfeeds")
+) {
+  if (obj?.data?.list?.length > 0) {
+    let list = obj.data.list;
+    const newList = [];
+    for (let item of list) {
+      if (item?.feedContent?.smallTags?.some((i) =>
+          i?.text?.includes("广告"))
+      ) {
+        continue;
+      }
+      if ([10002, 10003].includes(item?.feedType)) {
+        continue;
+      }
+      newList.push(item);
+    }
+    obj.data.list = newList;
+  }
+}
+$done({body: JSON.stringify(obj)})
+*****///
+
+
 
 body = JSON.stringify(obj);
 console.log(body);
