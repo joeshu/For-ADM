3fa148efd69d1159f964b5ecf6aa51f005b9dbf2
joeshu <350027830@qq.com>
2026-02-11 17:27:18 +0800

    Update tv0.js

--- tv0.js
+++ tv0.js
@@ -3,6 +3,6 @@
 [rewrite_local]
 ^https?:\/\/www\.555cin\.com\/(?!(.*(cdn-cgi|verify|auth|captch|(\.(js|css|jpg|jpeg|png|webp|gif|zip|woff|woff2|m3u8|mp4|mov|m4v|avi|mkv|flv|rmvb|wmv|rm|asf|asx|mp3|json|ico|otf|ttf))))).* url script-response-body https://limbopro.com/Adguard/Adblock4limbo.js
 
-//[mitm]
-//hostname = yzy0916.*.com,yz1018.*.com
+[mitm]
+hostname = *.555cin.com
 *************************************/
