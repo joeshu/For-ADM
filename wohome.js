4317ebf583982590dd2ea9a118c5de335f47039c
joeshu <350027830@qq.com>
2026-02-24 15:26:27 +0800

    Update wohome.js

--- wohome.js
+++ wohome.js
@@ -2,31 +2,33 @@
  * @function 联通智家
  * @date 2026-02-24
  [rewrite_local]
-^https:\/\/iotpservice\.smartont\.net\/wohome\/dispatcher url script-request-body https://raw.githubusercontent.com/joeshu/For-ADM/refs/heads/master/wohome.js
+^https:\/\/iotpservice\.smartont\.net\/wohome\/dispatcher url script-request-body https://raw.githubusercontent.com/joeshu/For-ADM/refs/heads/master/wohome.js 
 [mitm]
 hostname = iotpservice.smartont.net
  */
-// 修复：检查 request body 是否存在
-if (!request || !request.body) {
+
+// 修复：检查 $request.body 是否存在
+if (!$request || !$request.body) {
     $done({});
 }
 
-var body = $request.body;
-var obj;
+let body = $request.body;
+let obj;
 
 // 修复：使用 try-catch 解析 JSON
 try {
     obj = JSON.parse(body);
 } catch (e) {
     console.log("JSON parse error: " + e);
-    $done({});
+    return $done({});
 }
 
-// 修复：检查 obj.data 是否存在
-if (!obj || (obj.body.posCodeArr = "APP_START_PAGE") ) {
-        delete obj.body;
+// 修复：检查 obj.body 是否存在，并正确比较 posCodeArr
+if (obj && obj.body && obj.body.posCodeArr === "APP_START_PAGE") {
+    delete obj.body;
 }
 
 body = JSON.stringify(obj);
 console.log(body);
+
 $done({body: body});
