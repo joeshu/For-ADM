575a40ab2ff9441b910474fc8444058182ef7664
joeshu <350027830@qq.com>
2026-02-24 16:08:26 +0800

    Update wohome.js

--- wohome.js
+++ wohome.js
@@ -23,8 +23,9 @@ try {
 
 // 修复：检查 posCodeArr 数组是否包含 "APP_START_PAGE"
 if (obj?.body?.posCodeArr?.includes("APP_START_PAGE")) {
-    console.log("Before delete:", JSON.stringify(obj.body));
-    delete obj.body;
+    console.log("Before delete:", JSON.stringify(obj.body.posCodeArr));
+    //delete obj.body;
+     obj.body.posCodeArr = [];
     console.log("After delete:", JSON.stringify(obj));
 }
 
