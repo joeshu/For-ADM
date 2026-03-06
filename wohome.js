a5bcea774c0d6ab840b12834c465cb39afcebc89
joeshu <350027830@qq.com>
2026-03-07 01:13:12 +0800

    Update wohome.js

--- wohome.js
+++ wohome.js
@@ -1,10 +1,10 @@
 /**
  * @function 联通智家 - 去除开屏广告
  * @date 2026-02-24
-[rewrite_local]
- ^https:\/\/iotpservice\.smartont\.net\/wohome\/dispatcher url script-response-body https://raw.githubusercontent.com/joeshu/For-ADM/refs/heads/master/wohome.js
- [mitm]
- hostname = iotpservice.smartont.net
+ * [rewrite_local]
+ * ^https:\/\/iotpservice\.smartont\.net\/wohome\/dispatcher url script-response-body https://raw.githubusercontent.com/joeshu/For-ADM/refs/heads/master/wohome.js
+ * [mitm]
+ * hostname = iotpservice.smartont.net
  */
 
 if (!$response || !$response.body) {
@@ -21,14 +21,26 @@ try {
     $done({});
 }
 
-// 检查是否为开屏广告响应（posCode 是字符串 "APP_START_PAGE"）
-if (obj?.posCode === "APP_START_PAGE" && obj?.configList) {
-    console.log("Before modify:", JSON.stringify(obj));
+// 检查响应状态并处理开屏广告
+if (obj?.STATUS === "200" && obj?.RSP?.DATA?.length > 0) {
+    let modified = false;
     
-    // 清空广告配置列表，保留其他字段避免报错
-    obj.configList = [];
+    // 遍历 DATA 数组，查找开屏广告配置
+    for (let i = 0; i < obj.RSP.DATA.length; i++) {
+        let item = obj.RSP.DATA[i];
+        
+        if (item?.posCode === "APP_START_PAGE" && item?.configList) {
+            console.log(`Found APP_START_PAGE at index ${i}, removing ${item.configList.length} ads`);
+            
+            // 清空广告配置列表
+            item.configList = [];
+            modified = true;
+        }
+    }
     
-    console.log("After modify:", JSON.stringify(obj));
+    if (modified) {
+        console.log("After modify:", JSON.stringify(obj));
+    }
 }
 
 body = JSON.stringify(obj);
