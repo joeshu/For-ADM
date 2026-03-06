992b886fedf6f3654f45f95c66ca5b575fbeb2b0
joeshu <350027830@qq.com>
2026-03-07 01:03:32 +0800

    Update wohome.js

--- wohome.js
+++ wohome.js
@@ -1,11 +1,11 @@
 /**
- * @function 联通智家
+ * @function 联通智家 - 去除开屏广告
  * @date 2026-02-24
- [rewrite_local]
-^https:\/\/iotpservice\.smartont\.net\/wohome\/dispatcher url script-response-body https://raw.githubusercontent.com/joeshu/For-ADM/refs/heads/master/wohome.js   
- [mitm]
-hostname = iotpservice.smartont.net
- *****/
+ * [rewrite_local]
+ * ^https:\/\/iotpservice\.smartont\.net\/wohome\/dispatcher url script-response-body https://raw.githubusercontent.com/joeshu/For-ADM/refs/heads/master/wohome.js
+ * [mitm]
+ * hostname = iotpservice.smartont.net
+ */
 
 if (!$response || !$response.body) {
     $done({});
@@ -16,20 +16,20 @@ let obj;
 
 try {
     obj = JSON.parse(body);
- console.log("Before delete:", JSON.parse(obj));
 } catch (e) {
     console.log("JSON parse error: " + e);
     $done({});
 }
 
-// 修复：检查 posCodeArr 数组是否包含 "APP_START_PAGE"
-if (obj?.data?.posCode?.includes("APP_START_PAGE")) {
-    console.log("Before delete:", JSON.parse($request.body));
-    delete obj.data.configList;
-     //obj.body = {};
-    console.log("After delete:", JSON.parse($request.body));
+// 检查是否为开屏广告响应（posCode 是字符串 "APP_START_PAGE"）
+if (obj?.posCode === "APP_START_PAGE" && obj?.configList) {
+    console.log("Before modify:", JSON.stringify(obj));
+    
+    // 清空广告配置列表，保留其他字段避免报错
+    obj.configList = [];
+    
+    console.log("After modify:", JSON.stringify(obj));
 }
 
 body = JSON.stringify(obj);
-//console.log("Final body:", body);
 $done({ body: body });
