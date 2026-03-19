aeb8320eb8a358490f73591fe41d10d3ac0e3765
joeshu <350027830@qq.com>
2026-03-19 16:45:03 +0800

    Update Unified_VIP_Unlock_Manager_v6.js

--- Unified_VIP_Unlock_Manager_v6.js
+++ Unified_VIP_Unlock_Manager_v6.js
@@ -1025,7 +1025,7 @@ const APP_CONFIGS = Object.freeze({
         urlPattern: /^https?:\/\/.*v2ex\.com\/(?!(.*(api|login|cdn-cgi|verify|auth|captch|(\.(js|css|jpg|jpeg|png|webp|gif|zip|woff|woff2|m3u8|mp4|mov|m4v|avi|mkv|flv|rmvb|wmv|rm|asf|asx|mp3|json|ico|otf|ttf)))))/,
         htmlReplacements: [
             {
-                pattern: /<\/head>/i,
+                pattern: /<head>/i,
                 replacement:`<head><style>.sidebar_units,.sidebar_compliance,ins.adsbygoogle,div[class^="wwads-"]{display: none !important;}</style>`,
                 description: '注入CSS隐藏广告元素'
             }
